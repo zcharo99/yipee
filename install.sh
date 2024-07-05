6f552e1af88ca56e7cc800b09ff6abd73d4cc1ef
zChar <105929248+zcharo99@users.noreply.github.com>
2024-07-05 20:48:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 mkdir -p $HOME/.local/share/yipee
+curl https://raw.githubusercontent.com/zcharo99/yipee/main/aur > yipee
+echo ""
+sudo mv yipee /usr/bin/
+sudo chmod +x /usr/bin/yipee
+sudo chown $(whoami):$(whoami) /usr/bin/yipee
+echo ":: Installation complete!"
