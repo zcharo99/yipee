4b8bcc2675c420dc166f2163d9aa66d95047103d
zChar <105929248+zcharo99@users.noreply.github.com>
2024-07-06 15:35:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,29 @@
 checkpkg() {
   pacman -Q "$1" &>/dev/null
 }
-
-sudo rm -f /usr/bin/yipee &>/dev/null
-mkdir -p $HOME/.local/share/yipee
-curl https://raw.githubusercontent.com/zcharo99/yipee/main/aur > yipee
-echo ""
-sudo mv yipee /usr/bin/
-sudo chmod +x /usr/bin/yipee
-sudo chown $(whoami):$(whoami) /usr/bin/yipee
-
 if ! checkpkg git; then
- echo ":: Git is not installed, installing..."
- sudo pacman -Sy --noconfirm git
+ echo ":: git is not installed, installing..."
+ sudo pacman -Sy --noconfirm git &>/dev/null
  if [ $? -ne 0 ]; then
    echo "!! Failed to install git"
    exit 1
  fi
 fi
+
+if ! checkpkg git; then
+ echo ":: curl is not installed, installing..."
+ sudo pacman -Sy --noconfirm git &>/dev/null
+ if [ $? -ne 0 ]; then
+   echo "!! Failed to install curl"
+   exit 1
+ fi
+fi
+
+sudo rm -f /usr/bin/yipee &>/dev/null
+mkdir -p $HOME/.local/share/yipee
+curl -s https://raw.githubusercontent.com/zcharo99/yipee/main/aur > yipee
+echo ""
+sudo mv yipee /usr/bin/
+sudo chmod +x /usr/bin/yipee
 
 echo ":: Installation complete!"
